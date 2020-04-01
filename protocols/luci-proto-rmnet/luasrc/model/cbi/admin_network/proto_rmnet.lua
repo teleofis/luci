ccf7b669fa5062483350f5349b1bb719a38cf347
iuncuim <iuncuim@gmail.com>
2020-04-01 13:26:27 +0300

    RMNET: simman dependency

--- protocols/luci-proto-rmnet/luasrc/model/cbi/admin_network/proto_rmnet.lua
+++ protocols/luci-proto-rmnet/luasrc/model/cbi/admin_network/proto_rmnet.lua
@@ -9,14 +9,19 @@ local auth, ipv6, device
 device = section:taboption("general", Value, "device", translate("Interface name"))
 device.default = "rmnet_data0"
 
-apn = section:taboption("general", Value, "apn", translate("APN"))
-
-pincode = section:taboption("general", Value, "pincode", translate("PIN"))
-
-username = section:taboption("general", Value, "username", translate("PAP/CHAP username"))
-
-password = section:taboption("general", Value, "password", translate("PAP/CHAP password"))
-password.password = true
+local simman = map.uci:get("simman", "core", "enabled") or "0"
+if simman == "0" then
+	apn = section:taboption("general", Value, "apn", translate("APN"))
+	pincode = section:taboption("general", Value, "pincode", translate("PIN"))
+	username = section:taboption("general", Value, "username", translate("PAP/CHAP username"))
+	password = section:taboption("general", Value, "password", translate("PAP/CHAP password"))
+	password.password = true
+else
+	apn = section:taboption("general", DummyValue, "apn", translate("APN"), translate("You can configure this value in the SIM manager, or after disabling it"))
+	pincode = section:taboption("general", DummyValue, "pincode", translate("PIN"), translate("You can configure this value in the SIM manager, or after disabling it"))
+	username = section:taboption("general", DummyValue, "username", translate("PAP/CHAP username"), translate("You can configure this value in the SIM manager, or after disabling it"))
+	password = section:taboption("general", DummyValue, "password", translate("PAP/CHAP password"), translate("You can configure this value in the SIM manager, or after disabling it"))
+end	
 
 auth = section:taboption("general", Value, "auth", translate("Authentication Type"))
 auth:value("", translate("-- Please choose --"))
