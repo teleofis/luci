dc41451a8c9fc9429e723a4e225b479f16c3ea65
iuncuim <iuncuim@gmail.com>
2022-05-19 16:51:20 +0300

    luci-mod: voltage, ntpd, i18n

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/system.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/system.js
@@ -5,7 +5,6 @@
 'require uci';
 'require rpc';
 'require form';
-'require tools.widgets as widgets';
 
 var callInitList, callInitAction, callTimezone,
     callGetLocaltime, callSetLocaltime, CBILocalTime;
@@ -83,7 +82,7 @@ CBILocalTime = form.DummyValue.extend({
 				this.ntpd_support ? E('button', {
 					'class': 'cbi-button cbi-button-apply',
 					'click': ui.createHandlerFn(this, function() {
-						return callInitAction('sysntpd', 'restart');
+						return callInitAction('ntpd', 'restart');
 					}),
 					'disabled': (this.readonly != null) ? this.readonly : this.map.readonly
 				}, _('Sync with NTP-Server')) : ''
@@ -95,7 +94,7 @@ CBILocalTime = form.DummyValue.extend({
 return view.extend({
 	load: function() {
 		return Promise.all([
-			callInitList('sysntpd'),
+			callInitList('ntpd'),
 			callTimezone(),
 			callGetLocaltime(),
 			uci.load('luci'),
@@ -270,7 +269,7 @@ return view.extend({
 				else
 					uci.unset('system', 'ntp', 'enabled');
 
-				return callInitAction('sysntpd', 'enable');
+				return callInitAction('ntpd', 'enable');
 			};
 			o.load = function(section_id) {
 				return (ntpd_enabled == 1 &&
@@ -282,18 +281,8 @@ return view.extend({
 			o.ucisection = 'ntp';
 			o.depends('enabled', '1');
 
-			o = s.taboption('timesync', widgets.NetworkSelect, 'interface',
-				_('Bind NTP server'),
-				_('Provide the NTP server to the selected interface or, if unspecified, to all interfaces'));
+			o = s.taboption('timesync', form.Flag, 'use_gps', _('GPS Clock Synchronization'),_('if possible'));
 			o.ucisection = 'ntp';
-			o.depends('enable_server', '1');
-			o.multiple = false;
-			o.nocreate = true;
-			o.optional = true;
-
-			o = s.taboption('timesync', form.Flag, 'use_dhcp', _('Use DHCP advertised servers'));
-			o.ucisection = 'ntp';
-			o.default = o.enabled;
 			o.depends('enabled', '1');
 
 			o = s.taboption('timesync', form.DynamicList, 'server', _('NTP server candidates'));
