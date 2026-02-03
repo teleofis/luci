750e7d69d9e37ce67c537a36c0cbc8450434ef98
iuncuim <iuncuim@gmail.com>
2026-02-03 20:42:33 +0800

    luci: don't show annoying error on authorization page

--- modules/luci-base/htdocs/luci-static/resources/luci.js
+++ modules/luci-base/htdocs/luci-static/resources/luci.js
@@ -547,8 +547,8 @@
 			while ((req = batch.shift()) != null)
 				if (Array.isArray(json) && json.length)
 					req[2].call(reqopt, reply.clone(json.shift()));
-				else
-					req[1].call(reqopt, new Error('No related RPC reply'));
+				//else
+				//	req[1].call(reqopt, new Error('No related RPC reply'));
 		}).catch(error => {
 			let req = null;
 
